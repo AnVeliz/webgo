160bcc7d5320bf2decbc05ac5d6adae0ce99bc9e
Anatoliy Velizhanin <Anatoliy.Velizhanin@gmail.com>
2022-03-21 06:33:28 +0100

    disable context menu, code clean up

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"embed"
+	"errors"
 	"fmt"
 	"io"
 	"io/fs"
@@ -35,17 +36,7 @@ func main() {
 	log.Println("main file server handler has set up")
 
 	go func() {
-		time.Sleep(time.Duration(1 * time.Second))
-		chromiumTmpDir := createTemporaryChromium()
-		defer os.RemoveAll(chromiumTmpDir)
-
-		for _, file := range chromium.Files {
-			downloadFile(chromiumTmpDir, fmt.Sprintf("%s%s", address, file))
-		}
-
-		appRootFile := fmt.Sprintf("%s%s", address, "assets/webui/index.html")
-		cmd := exec.Command(path.Join(chromiumTmpDir, "assets/chromium/99.0.4844.74_x64/Chrome-bin/chrome.exe"), fmt.Sprintf("--app=%s", appRootFile))
-		cmd.Run()
+		runChromium(address)
 		srv.Shutdown(context.Background())
 	}()
 
@@ -54,6 +45,50 @@ func main() {
 		panic(err)
 	}
 	fmt.Println("main file server handler has stopped")
+}
+
+func runChromium(url string) error {
+	cmd, chromiumTmpDir := prepareChromiumCmd(url)
+	defer os.RemoveAll(chromiumTmpDir)
+
+	if !checkConnection(url, time.Duration(1*time.Second), 5) {
+		return errors.New("connection can not be established")
+	}
+
+	if err := cmd.Run(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func checkConnection(url string, timeout time.Duration, maxAttemptsNum int) bool {
+	httpClient := http.Client{
+		Timeout: timeout,
+	}
+
+	index := 0
+	for resp, err := httpClient.Get(url); err != nil || resp.StatusCode != http.StatusOK; index++ {
+		if index == maxAttemptsNum-1 {
+			fmt.Printf("can not establish connection error: %s", err)
+			return false
+		}
+		time.Sleep(timeout)
+	}
+	fmt.Printf("connected after %d attempt", index+1)
+
+	return true
+}
+
+func prepareChromiumCmd(address string) (*exec.Cmd, string) {
+	chromiumTmpDir := createTemporaryChromium()
+
+	for _, file := range chromium.Files {
+		downloadFile(chromiumTmpDir, fmt.Sprintf("%s%s", address, file))
+	}
+
+	appRootFile := fmt.Sprintf("%s%s", address, "assets/webui/index.html")
+	cmd := exec.Command(path.Join(chromiumTmpDir, "assets/chromium/99.0.4844.74_x64/Chrome-bin/chrome.exe"), fmt.Sprintf("--app=%s", appRootFile))
+	return cmd, chromiumTmpDir
 }
 
 func createTemporaryChromium() string {
